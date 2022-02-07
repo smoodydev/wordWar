7a400b087dff1fbb2c1541e2d694ce459fc1c94e
Stephen Moody <darilligames@gmail.com>
2022-02-07 15:07:24 +0000

    rough work through of basic design, started keyboard and divs

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
 import os
-from flask import Flask, session, jsonify
+from flask import Flask, session, jsonify, render_template, request
 from game_tools import word_new
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "somesecret")
 
-@app.route('/attempt')
-def attempt():
-    a = request.args.get('a', 0, type=int)
-    b = request.args.get('b', 0, type=int)
-    return jsonify(result=a + b)
+@app.route('/try_word', methods=["POST"])
+def try_word():
+    return jsonify(result="Something was sent and recieved?")
 
 @app.route('/new_word')
 def new_word():
     session["word"] = word_new(4)
-    return "Done"
+    return jsonify(result=session["word"])
 
 @app.route('/')
 def index():
     if "word" in session:
-        return 'Your word is ' + session["word"]
+        fill = 'Your word is ' + session["word"]
+        return render_template("index.html", fill=fill)
     else:
         session["word"] = word_new(4)
         return "First Timer? - Getting you a new word"
