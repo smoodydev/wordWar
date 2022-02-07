cf4c8d933fb4b6400b8d345aade89dc32ba7fa31
Stephen Moody <darilligames@gmail.com>
2022-02-07 16:52:38 +0000

    Created base try_solver, basic return ready

--- game_tools.py
+++ game_tools.py
@@ -5,3 +5,26 @@ def word_new(number):
         lines = full_dic.read().splitlines()
         word = random.choice(lines)
     return word
+
+def is_valid_word(word, num_letters):
+    with open(f'words-{str(num_letters)}.txt') as full_dic:
+        word = word.lower()
+        lines = full_dic.read().splitlines()
+        if word in lines:
+            return True
+        else:
+            return False
+
+def try_solve(word, attempt):
+    stringback = ""
+    print(word)
+    print(attempt)
+    for i, l in enumerate(attempt):
+        if l in word:
+            if l == word[i]:
+                stringback += "Y"
+            else:
+                stringback += "C"
+        else:
+            stringback += "N"
+    return stringback
--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 import os
 from flask import Flask, session, jsonify, render_template, request
-from game_tools import word_new
+from game_tools import word_new, is_valid_word, try_solve
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "somesecret")
 
 @app.route('/try_word', methods=["POST"])
 def try_word():
-    print(request.form["word"])
-    return jsonify(result="The server received the word"+request.form["word"])
+    word = request.form["word"].lower()
+
+    if is_valid_word(word, session["letters"]):
+        result = try_solve(session["word"], word)
+        return jsonify(result=result+ " The server received the word "+request.form["word"])
+    else:
+        return jsonify(result="Not a Valid Word")
 
 @app.route('/new_word')
 def new_word():
     session["word"] = word_new(5)
+    session["letters"] = 5
     return jsonify(result=session["word"])
 
 @app.route('/')
@@ -22,6 +28,7 @@ def index():
         return render_template("index.html", fill=fill)
     else:
         session["word"] = word_new(5)
+        session["letters"] = 5
         return "First Timer? - Getting you a new word"
     
 
